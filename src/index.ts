c7cb827744303d06a22acab58d2a340ee82a1f02
sua yoo <sua@suayoo.com>
2024-10-21 14:40:51 -0700

    format files

--- src/index.ts
+++ src/index.ts
@@ -9,10 +9,9 @@ import "./shoelace";
 const PAGE_COUNT_MIN = 4;
 
 @customElement("archive-now")
-class ArchiveNow extends LitElement
-{
+class ArchiveNow extends LitElement {
   @state()
-  private isFinished = false; 
+  private isFinished = false;
 
   @state()
   private collId = randomId();
@@ -27,7 +26,7 @@ class ArchiveNow extends LitElement
   private pageCount = 0;
 
   @query("sl-dialog")
-  private dialog? : SlDialog;
+  private dialog?: SlDialog;
 
   private currUrl = "";
   private shownForUrl = false;
@@ -43,22 +42,27 @@ class ArchiveNow extends LitElement
 
         case "live-proxy-url-error":
           if (event.data.status === 403) {
-            this.hintMessage = "It looks like this site is blocking us from loading it.";
+            this.hintMessage =
+              "It looks like this site is blocking us from loading it.";
           } else if (event.data.status > 500) {
-            this.hintMessage = "It looks like this might not be a valid URL or the site is down.";
+            this.hintMessage =
+              "It looks like this might not be a valid URL or the site is down.";
           } else if (event.data.status === 429) {
-            this.hintMessage = "It looks like you're been rate limited (by this site, not by us!).";
+            this.hintMessage =
+              "It looks like you're been rate limited (by this site, not by us!).";
           } else {
             this.hintMessage = "It looks like this page could not be loaded.";
           }
           break;
 
         case "rate-limited":
-          this.hintMessage = "It looks like you're been rate limited (by this site, not by us!).";
+          this.hintMessage =
+            "It looks like you're been rate limited (by this site, not by us!).";
           break;
 
         case "post-request-failed":
-          this.hintMessage = "It looks like you're trying to log in to a site or access social media.";
+          this.hintMessage =
+            "It looks like you're trying to log in to a site or access social media.";
           break;
 
         case "page-loading":
@@ -76,7 +80,10 @@ class ArchiveNow extends LitElement
           break;
       }
 
-      if ((this.hintMessage || this.pageCount > PAGE_COUNT_MIN) && !this.shownForUrl) {
+      if (
+        (this.hintMessage || this.pageCount > PAGE_COUNT_MIN) &&
+        !this.shownForUrl
+      ) {
         this.shownForUrl = true;
         this.dialog?.show();
       }
@@ -84,50 +91,69 @@ class ArchiveNow extends LitElement
   }
 
   render() {
-    return html`
-    ${!this.isFinished ? html`
-    <archive-web-page
-      proxyPrefix="https://archive-now.webrecorder.workers.dev/proxy/"
-      sandbox="true"
-      coll=${this.collId}
-      deepLink="true"
-      url="https://example.com/"
-    ></archive-web-page>` : html`
-    <replay-web-page
-      coll=${this.collId}
-      >`}
-    <div style="width: 300px">
-    ${!this.isFinished ? html`
-    <h2>High-Fidelity Archiving in your Browser</h2>
-    <p>Everything you see loaded on the left is being archived!</p>
+    return html` ${!this.isFinished
+        ? html` <archive-web-page
+            proxyPrefix="https://archive-now.webrecorder.workers.dev/proxy/"
+            sandbox="true"
+            coll=${this.collId}
+            deepLink="true"
+            url="https://example.com/"
+          ></archive-web-page>`
+        : html` <replay-web-page coll=${this.collId}></replay-web-page>`}
+      <div style="width: 300px">
+        ${!this.isFinished
+          ? html`
+              <h2>High-Fidelity Archiving in your Browser</h2>
+              <p>Everything you see loaded on the left is being archived!</p>
 
-    <p>When you're done, click <b>Finish</b></p>
-    ${this.renderDialogs()}
-    ` : html`
-    <h2>Replaying Archives</h2>
+              <p>When you're done, click <b>Finish</b></p>
+              ${this.renderDialogs()}
+            `
+          : html`
+              <h2>Replaying Archives</h2>
 
-    <p>Now, everything you see on the left is being loaded from the archive you just created.</p>
+              <p>
+                Now, everything you see on the left is being loaded from the
+                archive you just created.
+              </p>
 
-    <p><sl-button href="${this.downloadUrl}" target="_blank">Download your archive</sl-button></p>
-    `}
-    </div>`;
+              <p>
+                <sl-button href="${this.downloadUrl}" target="_blank"
+                  >Download your archive</sl-button
+                >
+              </p>
+            `}
+      </div>`;
   }
 
   renderDialogs() {
     return html`
-    <sl-dialog label="Suggestion" class="dialog-width" style="--width: 50vw;">
-      ${this.hintMessage ? html`
-      <p>${this.hintMessage}</p>
-      <p>This is an archiving demo that runs through a proxy and many things don't quite work.</p>
-      <p>For better results, try our ArchiveWeb.page extension.</p>` : ``}
-      ${this.pageCount > PAGE_COUNT_MIN ? html`
-      <p>It looks like you're trying to archive multiple pages. While it's possible to do it with this
-        demo, the Browsertrix service will make this a lot easier!
-      </p>
-          ` : ``}
-      <sl-button slot="footer" variant="primary" @click="${this.onCloseDialog}">Close</sl-button>
-    </sl-dialog>
-    `
+      <sl-dialog label="Suggestion" class="dialog-width" style="--width: 50vw;">
+        ${this.hintMessage
+          ? html` <p>${this.hintMessage}</p>
+              <p>
+                This is an archiving demo that runs through a proxy and many
+                things don't quite work.
+              </p>
+              <p>For better results, try our ArchiveWeb.page extension.</p>`
+          : ``}
+        ${this.pageCount > PAGE_COUNT_MIN
+          ? html`
+              <p>
+                It looks like you're trying to archive multiple pages. While
+                it's possible to do it with this demo, the Browsertrix service
+                will make this a lot easier!
+              </p>
+            `
+          : ``}
+        <sl-button
+          slot="footer"
+          variant="primary"
+          @click="${this.onCloseDialog}"
+          >Close</sl-button
+        >
+      </sl-dialog>
+    `;
   }
 
   onCloseDialog() {
